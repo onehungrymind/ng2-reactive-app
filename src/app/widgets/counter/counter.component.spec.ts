c8b0ab25ffe5aeb95166d4a9c04acee7bd543e8d
Jonathan Garvey <jon.d.garvey@gmail.com>
2016-11-04 23:01:41 -0700

    counter ui only

--- src/app/widgets/counter/counter.component.spec.ts
+++ src/app/widgets/counter/counter.component.spec.ts
@@ -1,18 +1,14 @@
 /* tslint:disable:no-unused-variable */
 
 import { TestBed, async, ComponentFixture } from '@angular/core/testing';
-import { ChangeDetectorRef } from '@angular/core';
 import { CounterComponent } from './counter.component';
-
-class ChangeDetectorRefStub {}
 
 describe('Component: Counter', () => {
   let fixture: ComponentFixture<CounterComponent>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [ CounterComponent ],
-      providers: [{ provide: ChangeDetectorRef, useClass: ChangeDetectorRefStub }]
+      declarations: [ CounterComponent ]
     });
 
     TestBed.overrideComponent(CounterComponent, {set: {template: ''}});
--- src/app/widgets/counter/counter.component.ts
+++ src/app/widgets/counter/counter.component.ts
@@ -1,9 +1,4 @@
-import { Component, ViewChild, ChangeDetectorRef, OnInit } from '@angular/core';
-import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/observable/merge';
-import 'rxjs/add/observable/fromEvent';
-import 'rxjs/add/operator/startWith';
-import 'rxjs/add/operator/scan';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-counter',
@@ -12,31 +7,8 @@ import 'rxjs/add/operator/scan';
 })
 export class CounterComponent implements OnInit {
 
-  @ViewChild('increment') increment;
-  @ViewChild('decrement') decrement;
-
-  coolness: number;
-
-  constructor(private cd: ChangeDetectorRef) {}
+  constructor() {}
 
   ngOnInit() {
-    const decrement$ = Observable.fromEvent(this.getNativeElement(this.decrement), 'click')
-      .map(event => -1);
-
-    const increment$ = Observable.fromEvent(this.getNativeElement(this.increment), 'click')
-      .map(event => 1);
-
-    Observable.merge(decrement$, increment$)
-      .startWith(5)
-      .scan((acc, curr) => acc + curr >= 0 ? acc + curr : acc)
-      .subscribe(coolness => {
-        this.coolness = coolness;
-        this.cd.detectChanges();
-      });
-
-  }
-
-  getNativeElement(element) {
-    return element.nativeElement;
   }
 }
