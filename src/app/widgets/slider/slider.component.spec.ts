cc2ddc6b21579582c9ab3659017c056d0b05db95
Micah Torres <micahtorres@Micahs-MacBook-Pro.local>
2017-04-26 13:55:30 -0700

    fixed broken test

--- src/app/widgets/slider/slider.component.spec.ts
+++ src/app/widgets/slider/slider.component.spec.ts
@@ -2,10 +2,11 @@
 
 import { TestBed, async } from '@angular/core/testing';
 import { SliderComponent } from './slider.component';
+import { FormBuilder } from '@angular/forms';
 
 describe('Component: Slider', () => {
   it('should create an instance', () => {
-    let component = new SliderComponent();
+    let component = new SliderComponent(new FormBuilder());
     expect(component).toBeTruthy();
   });
 });
